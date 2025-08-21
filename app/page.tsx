70b5e19ebf5b7bdc0839ba30a4a798bddef0ac3f
TranKhanh1307 <haxigi1307@gmail.com>
2025-08-21 10:46:06 +0700

    fix landing page UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { rightArrowIcon } from "./ui/common/icons";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
+    <main className="flex h-full flex-col p-6">
       <AcmeLogo />
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
